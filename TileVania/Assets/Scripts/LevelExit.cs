d35c0605937f8c3ebc0622e0a4cb93f8d2ca8568
Bryan Fugere <45855115+bfugere@users.noreply.github.com>
2021-09-21 16:10:51 -0400

    Level Exit Portals

---
+++ TileVania/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 2f;
+    [SerializeField] float levelExitSlowMoEffect = 0.1f;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        Time.timeScale = levelExitSlowMoEffect;
+        
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        Time.timeScale = 1f;
+
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
